925da27bd8b67c07391abec511d0c4b9198de906
Chezter Reblando <144419275+superchezzz@users.noreply.github.com>
2024-01-27 01:02:45 +0800

    Create whatIf.c
    haha wala lang

---
+++ whatIf.c
@@ -0,0 +1,405 @@
+#include <stdio.h>
+
+int stars(int i);
+int space(int s);
+
+int main(){
+    //Line 1
+    stars(1);
+    space(5);
+    stars(1);   
+    printf("\n");
+    
+    //Line 2
+    stars(1);
+    space(2);
+    stars(1);
+    space(2);
+    stars(1);
+    space(1);
+    stars(1);
+    space(4);
+    stars(1);
+    space(3);
+    stars(2);
+    space(3);
+    stars(4);
+    space(4);
+    stars(1);
+    space(1);
+    stars(6);
+    printf("\n");
+
+    //Line 3
+    stars(1);
+    space(2);
+    stars(1);
+    space(2);
+    stars(1);
+    space(1);
+    stars(1);
+    space(4);
+    stars(1);
+    space(2);
+    stars(1);
+    space(2);
+    stars(1);
+    space(4);
+    stars(1);
+    space(5);
+    stars(1);
+    space(1);
+    stars(1);
+    printf("\n");
+
+    //Line 4
+    stars(1);
+    space(2);
+    stars(1);
+    space(2);
+    stars(1);
+    space(1);
+    stars(6);
+    space(1);
+    stars(1);
+    space(4);
+    stars(1);
+    space(3);
+    stars(1);
+    space(5);
+    stars(1);
+    space(1);
+    stars(5);
+    printf("\n");
+
+    //Line 5
+    stars(1);
+    space(2);
+    stars(1);
+    space(2);
+    stars(1);
+    space(1);
+    stars(1);
+    space(4);
+    stars(1);
+    space(1);
+    stars(6);
+    space(3);
+    stars(1);
+    space(5);
+    stars(1);
+    space(1);
+    stars(1);
+    printf("\n");
+    
+    //Line 6
+    stars(1);
+    space(2);
+    stars(1);
+    space(2);
+    stars(1);
+    space(1);
+    stars(1);
+    space(4);
+    stars(1);
+    space(1);
+    stars(1);
+    space(4);
+    stars(1);
+    space(3);
+    stars(1);
+    space(5);
+    stars(1);
+    space(1);
+    stars(1);
+    printf("\n");
+
+    //Line 7
+    space(1);
+    stars(2);
+    space(1);
+    stars(2);
+    space(2);
+    stars(1);
+    space(4);
+    stars(1);
+    space(1);
+    stars(1);
+    space(4);
+    stars(1);
+    space(3);
+    stars(1);
+    space(5);
+    stars(1);
+    space(1);
+    stars(1);
+    printf("\n\n");
+
+    //Line 9
+    stars(1);
+    space(1);
+    stars(5);
+    space(1);
+    stars(5);
+    space(2);
+    stars(1);
+    space(3);
+    stars(1);
+    space(4);
+    stars(1);
+    space(4);
+    stars(1);
+    space(1);
+    stars(1);
+    space(6);
+    stars(1);
+    space(1);
+    stars(5);
+    printf("\n");
+
+    //Line 10
+    stars(1);
+    space(3);
+    stars(1);
+    space(3);
+    stars(1);
+    space(4);
+    stars(1);
+    space(2);
+    stars(1);
+    space(1);
+    stars(1);
+    space(5);
+    stars(1);
+    space(4);
+    stars(1);
+    space(1);
+    stars(1);
+    space(6);
+    stars(1);
+    space(3);
+    stars(1);
+    printf("\n");
+
+    //Line 11
+    stars(1);
+    space(3);
+    stars(1);
+    space(3);
+    stars(1);
+    space(4);
+    stars(1);
+    space(3);
+    stars(1);
+    space(6);
+    stars(1);
+    space(4);
+    stars(1);
+    space(1);
+    stars(1);
+    space(6);
+    stars(1);
+    space(3);
+    stars(1);
+    printf("\n");
+    //Line 12
+    stars(1);
+    space(3);
+    stars(1);
+    space(3);
+    stars(5);
+    space(4);
+    stars(1);
+    space(6);
+    stars(1);
+    space(4);
+    stars(1);
+    space(1);
+    stars(1);
+    space(6);
+    stars(1);
+    space(3);
+    stars(1);
+    printf("\n");
+
+    //Line 13
+    stars(1);
+    space(3);
+    stars(1);
+    space(3);
+    stars(1);
+    space(3);
+    stars(1);
+    space(4);
+    stars(1);
+    space(6);
+    stars(1);
+    space(4);
+    stars(1);
+    space(1);
+    stars(1);
+    space(6);
+    stars(1);
+    space(3);
+    stars(1);
+    printf("\n");
+
+    //Line 14
+    stars(1);
+    space(3);
+    stars(1);
+    space(3);
+    stars(1);
+    space(4);
+    stars(1);
+    space(3);
+    stars(1);
+    space(7);
+    stars(4);
+    space(2);
+    stars(6);
+    space(1);
+    stars(1);
+    space(3);
+    stars(1);
+    printf("\n\n");
+
+    //Line 17
+    space(30);
+    stars(5);
+    printf("\n");
+
+    //Line 18
+    stars(1);
+    space(4);
+    stars(1);
+    space(3);
+    stars(2);
+    space(3);
+    stars(5);
+    space(1);
+    stars(1);
+    space(1);
+    stars(1);
+    space(4);
+    stars(1);
+    space(1);
+    stars(1);
+    space(5);   
+    stars(1);
+    printf("\n");
+    //Line 19
+    stars(2);
+    space(3);
+    stars(1);
+    space(2);
+    stars(1);
+    space(2);
+    stars(1);
+    space(4);
+    stars(1);
+    space(3);
+    stars(1);
+    space(1);
+    stars(2);
+    space(3);
+    stars(1);
+    space(6);
+    stars(1);
+    printf("\n");
+
+    //Line 20
+    stars(1);
+    space(1);
+    stars(1);
+    space(2);
+    stars(1);
+    space(1);
+    stars(1);
+    space(4);
+    stars(1);
+    space(3);
+    stars(1);
+    space(3);
+    stars(1);
+    space(1);
+    stars(1);
+    space(1);
+    stars(1);
+    space(2);
+    stars(1);
+    space(4);
+    stars(3);
+    printf("\n");
+    //Line 21
+    stars(1);
+    space(2);
+    stars(1);
+    space(1);
+    stars(1);
+    space(1);
+    stars(6);
+    space(3);
+    stars(1);
+    space(3);
+    stars(1);
+    space(1);
+    stars(1);
+    space(2);
+    stars(1);
+    space(1);
+    stars(1);
+    space(4);
+    stars(1);
+    printf("\n");
+    //Line 22
+    stars(1);
+    space(3);
+    stars(2);
+    space(1);
+    stars(1);
+    space(4);
+    stars(1);
+    space(3);
+    stars(1);
+    space(3);
+    stars(1);
+    space(1);
+    stars(1);
+    space(3);
+    stars(2);
+    printf("\n");
+    //Line 23
+    stars(1);
+    space(4);
+    stars(1);
+    space(1);
+    stars(1);
+    space(4);
+    stars(1);
+    space(3);
+    stars(1);
+    space(3);
+    stars(1);
+    space(1);
+    stars(1);
+    space(4);
+    stars(1);
+    space(4);
+    stars(1);
+    printf("\n");
+}
+
+int stars(int i){
+    for(int a=1;a<=i; a++)
+    {
+         printf("*");
+    }
+}
+int space(int s){
+    for(int b=1;b<=s; b++)
+    {
+         printf(" ");
+    }
+    return 0;
+}
